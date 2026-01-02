4265701e81fd88431ff56df0d617c3db66358224
PRINCE TECH <majidiabduli2003@gmail.com>
2026-01-02 21:21:58 +0300

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𝙿𝚁𝙸𝙽𝙲𝙴 𝚇𝙼𝙳 𝙱𝙾𝚃',
+  author: '‎',
+  botName: "𝙿𝚁𝙸𝙽𝙲𝙴 𝚇𝙼𝙳 𝙱𝙾𝚃",
+  botOwner: '𝙿𝚁𝙸𝙽𝙲𝙴', // Your name
+  ownerNumber: '255747311795', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.5",
+  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+};
+
+module.exports = settings;
